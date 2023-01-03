e8f20485918ac60f7817e3bdb1c788d95f13e0df
Diego Assunção <diegoadler2008@gmail.com>
2023-01-03 16:25:05 -0300

    up

---
+++ desafio002.py
@@ -0,0 +1,5 @@
+nome = input('Digite seu nome! ')
+dia = input('Digite o dia que você nasceu ')
+mes = input('Digite o mês que você nasceu ')
+ano = input('Digite o ano que você nasceu ')
+print('Oi, ', nome, 'você nasceu no dia ', dia , 'do mes de', mes , 'e do ano de ', ano)
