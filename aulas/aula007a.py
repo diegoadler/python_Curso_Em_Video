d53fdf8f8c1d4f1dfe5d324302d75bb193bf6f5c
Diego Assunção <diegoadler2008@gmail.com>
2023-01-10 13:39:12 -0300

    atualização

--- aulas/aula007a.py
+++ aulas/aula007a.py
@@ -1,8 +1,10 @@
-n1 = input('Digite um numero')
-n2 = input('digite outro numero')
+n1 = int(input('Digite um numero: '))
+n2 = int(input('digite outro numero: '))
 s = n1 + n2
 m = n1 + n2
 d = n1 + n2
-di = n1 + n2
+di = n1 // n2
 e = n1 ** n2
-print('a soma vale {}'.format(n1+n2))
+print('a soma é {}, o produto é {} e a divisão é {:.3f}'.format(s, m, d), end='')
+print('A divisão inteira {} e potência {}'.format(di, e ))
+
